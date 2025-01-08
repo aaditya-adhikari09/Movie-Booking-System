951a41460d11eeae8f503b4908bca4912e65fb9a
Aditya Adhikari <134985776+aaditya-adhikari09@users.noreply.github.com>
2025-01-08 22:28:07 +0545

    Delete Api directory
    i will reupload

--- Api/admin.py
+++
@@ -1,9 +0,0 @@
-# from django.contrib import admin
-
-# # Register your models here.
-# from .models import User 
-
-
-# @admin.register(User)
-# class User(admin.ModelAdmin):
-#     list_display =['id',"full_name","email","full_address","role","bio","phone"]
